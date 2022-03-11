8d9034ac6b8fd5bead9c44a42ad6bb3ee058ae2b
Mohammed Abbas <101219974+MQABBAS@users.noreply.github.com>
2022-03-11 23:33:58 +0000

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,10 @@
+# Main String
+number = int(input("enter a number"))
+remainder = number % 2
+
+# If and else statement
+
+if (remainder == 0):
+    print(number, "is an even number")
+else: 
+    print(number,"is an odd number")
